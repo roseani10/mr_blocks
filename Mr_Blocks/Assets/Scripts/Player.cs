408e5fe9e4eac816c33546dd90b842ad2d791911
roseani10 <roseanirudh@gmail.com>
2025-06-06 22:26:39 +0530

    Input and movement of player

--- Mr_Blocks/Assets/Scripts/Player.cs
+++ Mr_Blocks/Assets/Scripts/Player.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float horizontalInput, verticalInput;
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("I am Mr. Blocks!");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+        MovePlayer();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        Debug.Log($"Horizontal: {horizontalInput}, Vertical: {verticalInput}");
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0);
+        Vector3 moveDelta = moveDirection.normalized * speed * Time.deltaTime;
+        transform.position += moveDelta;
     }
 }
