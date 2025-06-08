036b5d0173d12f9739d2f058fc039a6ac628a611
roseani10 <roseanirudh@gmail.com>
2025-06-08 22:42:09 +0530

    Update SpikeBall.cs

--- Mr_Blocks/Assets/Scripts/Obstacles/SpikeBall.cs
+++ Mr_Blocks/Assets/Scripts/Obstacles/SpikeBall.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SpikeBall : MonoBehaviour
 {
+
+    void Start()
+    {
+        Debug.Log("Start Called!");
+    }
     public float rotationAngle = 90f;
     private void Update() => RotateSpikeBall();
     private void RotateSpikeBall() => transform.Rotate(Vector3.forward, rotationAngle);
