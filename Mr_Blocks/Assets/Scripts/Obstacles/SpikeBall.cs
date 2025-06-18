d91e95deb45c66cdc1d5a7293d8ef2bf6f29751d
roseani10 <roseanirudh@gmail.com>
2025-06-18 22:44:14 +0530

    Update SpikeBall.cs

--- Mr_Blocks/Assets/Scripts/Obstacles/SpikeBall.cs
+++ Mr_Blocks/Assets/Scripts/Obstacles/SpikeBall.cs
@@ -7,24 +7,24 @@ public class SpikeBall : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("Start Called!");
+        //Debug.Log("Start Called!");
         DoSomething();
     }
 
     void DoSomething()
     {
-        Debug.Log("Doing it first time.");
+        //Debug.Log("Doing it first time.");
         RotateSpikeBall();
     }
     public float rotationAngle = 90f;
     private void Update()
     {
-        Debug.Log("Inside Update.");
+        //Debug.Log("Inside Update.");
         RotateSpikeBall();
     }
     private void RotateSpikeBall()
     {
-        Debug.Log("Inside Rotate Spike Ball.");
+        //Debug.Log("Inside Rotate Spike Ball.");
         transform.Rotate(Vector3.forward, rotationAngle * Time.deltaTime);
     }
 }
